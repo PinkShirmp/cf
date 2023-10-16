d54c2769ae15d7a76c48a967e0fdb4fd558f426d
PinkShirmp <nminhtue094@gmail.com>
2023-10-16 23:19:24 +0700

    Added Vaction.cpp

---
+++ Vacation.cpp
@@ -0,0 +1,151 @@
+/*
+ く__,.ヘヽ.　　　　/　,ー､ 〉
+　　　　　＼ ', !-─‐-i　/　/´
+　　　 　 ／｀ｰ'　　　 L/／｀ヽ､
+　　 　 /　 ／,　 /|　 ,　 ,　　　 ',
+　　　ｲ 　/ /-‐/　ｉ　L_ ﾊ ヽ!　 i
+　　　 ﾚ ﾍ 7ｲ｀ﾄ　 ﾚ'ｧ-ﾄ､!ハ|　 |
+　　　　 !,/7 '0'　　 ´0iソ| 　      |　　　
+　　　　 |.从"　　_　　 ,,,, / |./ 　 |
+　　　　 ﾚ'| i＞.､,,__　_,.イ / 　.i 　|
+　　　　　 ﾚ'| | / k_７_/ﾚ'ヽ,　ﾊ.　|
+　　　　　　 | |/i 〈|/　 i　,.ﾍ |　i　|
+　　　　　　.|/ /　ｉ： 　 ﾍ!　　＼　|
+　　　 　 　 kヽ>､ﾊ 　 _,.ﾍ､ 　 /､!
+　　　　　　 !'〈//｀Ｔ´', ＼ ｀'7'ｰr'
+　　　　　　 ﾚ'ヽL__|___i,___,ンﾚ|ノ
+　　　　　 　　　ﾄ-,/　|___./
+　　　　　 　　　'ｰ'　　!_,.:
+              Author: Solirous
+*/ 
+#include <bits/stdc++.h>
+using namespace std;
+void __print(int x) { cerr << x; }
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+template <typename A>
+void __print(const A &x);
+template <typename A, typename B>
+void __print(const pair<A, B> &p);
+template <typename... A>
+void __print(const tuple<A...> &t);
+template <typename T>
+void __print(stack<T> s);
+template <typename T>
+void __print(queue<T> q);
+template <typename T, typename... U>
+void __print(priority_queue<T, U...> q);
+template <typename A>
+void __print(const A &x) {
+    bool first = true;
+    cerr << '{';
+    for (const auto &i : x) {
+        cerr << (first ? "" : ","), __print(i);
+        first = false;
+    }
+    cerr << '}';
+}
+template <typename A, typename B>
+void __print(const pair<A, B> &p) {
+    cerr << '(';
+    __print(p.first);
+    cerr << ',';
+    __print(p.second);
+    cerr << ')';
+}
+template <typename... A>
+void __print(const tuple<A...> &t) {
+    bool first = true;
+    cerr << '(';
+    apply([&first](const auto &...args) { ((cerr << (first ? "" : ","), __print(args), first = false), ...); }, t);
+    cerr << ')';
+}
+template <typename T>
+void __print(stack<T> s) {
+    vector<T> debugVector;
+    while (!s.empty()) {
+        T t = s.top();
+        debugVector.push_back(t);
+        s.pop();
+    }
+    reverse(debugVector.begin(), debugVector.end());
+    __print(debugVector);
+}
+template <typename T>
+void __print(queue<T> q) {
+    vector<T> debugVector;
+    while (!q.empty()) {
+        T t = q.front();
+        debugVector.push_back(t);
+        q.pop();
+    }
+    __print(debugVector);
+}
+template <typename T, typename... U>
+void __print(priority_queue<T, U...> q) {
+    vector<T> debugVector;
+    while (!q.empty()) {
+        T t = q.top();
+        debugVector.push_back(t);
+        q.pop();
+    }
+    __print(debugVector);
+}
+void _print() { cerr << "]\n"; }
+template <typename Head, typename... Tail>
+void _print(const Head &H, const Tail &...T) {
+    __print(H);
+    if (sizeof...(T))
+        cerr << ", ";
+    _print(T...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(...) cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; _print(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+#define fr(i,start,end) for(int i=start;i<end;i++)
+#define fre(x,arr) for(auto x : arr)
+int n,a[101],dp[101][3];
+// dp[i][j] maxium of non-resting day if i day passed and j {0:rest,1:do contest:2 do contest}
+int main(){
+  cin.tie(0)->sync_with_stdio(0);
+  cin>>n;
+  fr(i,1,n+1) cin>>a[i];
+  dp[1][0]=0;
+  if(a[1]==1){
+    dp[1][1]=1;
+  }
+  if(a[1]==2){
+    dp[1][2]=1;
+  }
+  if(a[1]==3){
+    dp[1][1]=1;
+    dp[1][2]=1;
+  }
+  fr(i,1,n+1){
+    if(a[i]==0){
+      dp[i][0]=max(dp[i-1][0],dp[i-1][1]);
+      dp[i][0]=max(dp[i][0],dp[i-1][2]);
+    }else if(a[i]==1){
+      dp[i][1]=max(dp[i-1][0]+1,dp[i-1][2]+1);
+    }else if(a[i]==2){
+      dp[i][2]=max(dp[i-1][0]+1,dp[i-1][1]+1);
+    }else{
+      dp[i][1]=max(dp[i-1][0]+1,dp[i-1][2]+1);
+      dp[i][2]=max(dp[i-1][0]+1,dp[i-1][1]+1);
+    }
+  }
+  cout<<n-max(max(dp[n][0],dp[n][1]),dp[n][2])<<"\n";;
+}
+
